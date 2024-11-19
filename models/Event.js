45cb7a8d67edb0ee380bd85cc2826e9e9c3bbbe0
Vibhu Dixit <vibhudixit.2215001940.com>
2024-11-19 18:54:34 +0530

    beautify code

--- models/Event.js
+++ models/Event.js
@@ -4,10 +4,9 @@ const eventSchema = new mongoose.Schema({
     name: { type: String, required: true },
     date: { type: Date, required: true },
     location: { type: String, required: true },
-    description: { type: String, required: true }, // Ensure this is included
-    venue: { type: String, required: true },       // Ensure this is included
-    price: { type: Number, required: true }        // Ensure this is included
-    // Add other fields as needed
+    description: { type: String, required: true }, 
+    venue: { type: String, required: true },       
+    price: { type: Number, required: true }
 });
 
 module.exports = mongoose.model('Event', eventSchema);
