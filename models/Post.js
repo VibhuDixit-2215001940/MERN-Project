c39efbc4c31edb67cf487584fd71e32d19778961
Vibhu Dixit <vibhudixit.2215001940@gmail.com>
2024-12-01 23:33:19 +0530

    add complete

--- models/Post.js
+++ models/Post.js
@@ -1,19 +1,19 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const postSchema = new Schema({
-    userId: { type: Schema.Types.ObjectId, ref: 'User', required: true }, // Reference to the user who created the post
-    content: { type: String, required: true }, // Text content of the post
-    createdAt: { type: Date, default: Date.now }, // Date of creation
-    image: { type: String }, // Path to an image if the post has one
-    likes: { type: Number, default: 0 }, // Count of likes
+    userId: { type: Schema.Types.ObjectId, ref: 'User', required: true }, 
+    content: { type: String, required: true }, 
+    createdAt: { type: Date, default: Date.now }, 
+    image: { type: String }, 
+    likes: { type: Number, default: 0 }, 
     comments: [
         {
-            userId: { type: Schema.Types.ObjectId, ref: 'User' }, // Reference to the user who commented
-            comment: { type: String, required: true }, // Comment text
-            createdAt: { type: Date, default: Date.now } // Date of the comment
+            userId: { type: Schema.Types.ObjectId, ref: 'User' }, 
+            comment: { type: String, required: true }, 
+            createdAt: { type: Date, default: Date.now } 
         }
     ],
-    shares: { type: Number, default: 0 } // Count of shares
+    shares: { type: Number, default: 0 } 
 });
 
 module.exports = mongoose.model('Post', postSchema);
