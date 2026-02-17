29cbd644caddff4a3b2c15cad56e49d864573174
vibhu-dixit-dev <vibhu.dixit@onmeridian.com>
2026-02-17 22:53:33 +0530

    Replace bcrypt with bcryptjs for Azure Windows compatibility

--- models/BusinessUser.js
+++ models/BusinessUser.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 const BusinessUserSchema = new mongoose.Schema({username: {type: String,required: true,unique: true},password: {type: String,required: true},purchases: {type: Number,default: 0 },sales: {type: Number,default: 0 },profit: {type: Number,default: 0 },lastLogin: { type: Date, default: Date.now },
 });
